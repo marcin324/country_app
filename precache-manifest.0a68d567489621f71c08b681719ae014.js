8feb46ad0e1499081a238ebf62e7336225756468
Marcin Olszowski <olszyna38@gmail.com>
2019-05-28 10:33:32 +0200

    Updates

---
+++ precache-manifest.0a68d567489621f71c08b681719ae014.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/country_app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "c28c86e4e39ae77482cc",
+    "url": "/country_app/static/css/main.8bfa9695.chunk.css"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/country_app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "9cbd39561040b8552d0c",
+    "url": "/country_app/static/js/1.9cbd3956.chunk.js"
+  },
+  {
+    "revision": "a4041afdb123c25fb6c9",
+    "url": "/country_app/static/js/runtime~main.a4041afd.js"
+  },
+  {
+    "revision": "c28c86e4e39ae77482cc",
+    "url": "/country_app/static/js/main.c28c86e4.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/country_app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/country_app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/country_app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "9cbd39561040b8552d0c",
+    "url": "/country_app/static/css/1.c637eca9.chunk.css"
+  },
+  {
+    "revision": "1bd9a8f1b2daacfbaafcec1ad4be5342",
+    "url": "/country_app/index.html"
+  }
+];
